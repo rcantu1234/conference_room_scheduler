348bb87237962bb0733337aa5e5bf9a12f457631
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-19 18:10:57 -0600

    Added seed file and formatted table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+  User.create({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    date_of_birth: "#{n}/#{n}/#{n}",
+    # email: "test#{n}@example.com",
+    # password: "password"
+    })
+end
+
+
+
