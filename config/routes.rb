11d4b0e1f358c91e2e8ce6a46a528dc76fa32da8
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-21 10:02:43 -0600

    Added Welcome route and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
 
   resources :rooms do
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'users#index'
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
