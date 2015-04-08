0760b9e5b3783467799c486f3697f6a494ad1b79
ROEL CANTU <roel.cantu@yahoo.com>
2015-04-07 22:34:14 -0500

    Formatted photos in index page

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,7 +5,6 @@ class Room < ActiveRecord::Base
 
   has_attached_file :photo, styles: { large: "600x600>", :medium => "300x300>", :thumb => "150x150#" }
 
-
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
 end
